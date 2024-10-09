f7b464af61bde8ed55092968e51f73a817a950c7
barryZZJ <850462618@qq.com>
2024-10-09 11:44:19 +0800

    update: host url

--- rss_monitor/check_rss_available.py
+++ rss_monitor/check_rss_available.py
@@ -1,6 +1,6 @@
 import requests as r
 
-def is_rss_available(path, url='http://yy.barryzzj.asia:23200', timeout=10):
+def is_rss_available(path, url='http://yy.barryzzj.top:23200', timeout=10):
     try:
         resp = r.get(url + path, timeout=timeout)
         return resp.ok
