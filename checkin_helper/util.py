01b31095d0e92d38d86ce167bcdd7076ae7b64a0
barryZZJ <850462618@qq.com>
2024-12-07 16:19:56 +0800

    add: report textcard if content provided

--- checkin_helper/util.py
+++ checkin_helper/util.py
@@ -6,10 +6,9 @@ from config import load_conf
 
 def report(title, content=None):
     logger.info('title: {}\ncontent: {}', title, content)
-    if content is not None:
-        msg = f'{title=}\n{content=}'
-    else:
-        msg = title
     conf = load_conf()
     bot = wecomsan.WecomSan(**conf['bot'])
-    bot.send(msg)
+    if content is not None:
+        bot.send_textcard(title, content, 'http://myflaskserver.barryzzj.top:23354/test')
+    else:
+        bot.send(title)
